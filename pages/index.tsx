28f837504124548df19dde92bd48c26750620443
kohj1018 <kohj1018@hanyang.ac.kr>
2023-01-11 23:26:52 +0900

    [add][EA-59] pretendard 폰트 적용

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,15 @@ import { NextPage } from 'next'
 
 const Home: NextPage = () => {
   return (
+    <div>
       <h1 className="text-3xl font-bold underline">
         Hello world!
       </h1>
+      <br/>
+      <h1 className="text-2xl">
+        안녕하세요! 저는 바보입니다.
+      </h1>
+    </div>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -5,7 +7,14 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        pretendard: ['Pretendard', ...defaultTheme.fontFamily.sans]
+      },
+      boxShadow: {
+        'sm-custom': '0px 1px 24px rgba(0, 0, 0, 0.04)'
+      }
+    },
   },
   plugins: [],
 }
