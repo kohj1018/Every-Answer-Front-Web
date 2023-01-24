eac83c888bf5ecd3abf5eaa527cbb08cbdf2f97e
kohj1018 <kohj1018@gmail.com>
2023-01-24 19:35:44 +0900

    baseurl 변경 (https 설정)

--- utils/apis/apiConfig.ts
+++ utils/apis/apiConfig.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
-const BASE_URL = 'http://ec2-3-34-229-56.ap-northeast-2.compute.amazonaws.com:8080/api/v1'
+const BASE_URL = 'https://everyanswer.xyz/api/v1'
 
 export const ec2 = axios.create({ baseURL: BASE_URL })
